78f49e4ab9b139d8667ca233e1c94409be5299fc
Birte05 <63093822+Birte05@users.noreply.github.com>
2020-12-24 11:07:50 +0100

    added alerts

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react';
+import { ErrorAlert } from './Alert';
+
 
 class NumberOfEvents extends Component {
   state = {
     numberOfEvents: 32,
   };
 
-  // handleInputChanged = (event) => {
-  //   const value = event.target.value;
-  //   this.props.updateEvents(null, value);
-  //   this.setState({ numberOfEvents: value });
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    this.setState({ numberOfEvents: value });
 
-  //   if (value < 1) {
-  //     this.setState({
-  //       infoText: "Select number from 1 to 32",
-  //     });
-  //   } else {
-  //     this.setState({
-  //       infoText: "",
-  //     });
-  //   }
-  // };
+    if (event.target.value > 0 && event.target.value < 33) {
+      this.setState({
+        infoText: "Select number from 1 to 32",
+      });
+    } else {
+      this.setState({
+        infoText: "",
+      });
+    }
+  };
 
 
   render() {
     const { numberOfEvents, handleEventCount } = this.props;
     return <div>
-
-      <div className="NumberOfEvents">
+        <ErrorAlert text={this.state.infoText} />
+      <div className="numberOfEvents">
         <label className="numberOfEvents">Number of events</label>
         <input
           type="text"
@@ -34,7 +35,6 @@ class NumberOfEvents extends Component {
           value={numberOfEvents}
           onChange={handleEventCount}
         />
-
       </div>
     </div>
   }
