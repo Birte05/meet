33e428b450bf423709123e74ebc9ee72f188c1cb
Birte05 <63093822+Birte05@users.noreply.github.com>
2020-12-25 15:02:53 +0100

    added service worker

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './App.css';
 import App from './App';
-//import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker';
 import * as atatus from 'atatus-spa';
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -11,5 +11,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-//serviceWorker.unregister();
+serviceWorker.register();
 atatus.config('f3dd420b22c04fc386c9fb2edfe48907').install();
